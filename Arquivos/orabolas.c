4b1e719f3b9dfe7400519b32ef46baacc1caabb4
ArthurSousa11 <97888417+ArthurSousa11@users.noreply.github.com>
2022-05-19 20:01:10 -0300

    Update orabolas.c

--- Arquivos/orabolas.c
+++ Arquivos/orabolas.c
@@ -2,26 +2,40 @@
 #include <stdlib.h>
 #include <math.h>
 
-// Calcula distancia entre o robo e a bola
-double calcDistRoboBola (double xR, double yR, double xB, double yB, double tempo){
+int spaceEqX(float finalSpaceX, float initialSpaceX, float initialVelocity, float time) {
+    float accelX;
+    accelX = ((finalSpaceX * 2) - initialSpaceX - (initialVelocity * time)) / (time * time);
+    return accelX;
+}
+
+int spaceEqY(float finalSpaceY, float initialSpaceY, float initialVelocity, float time, float accelY) {
+    accelY = ((finalSpaceY * 2) - initialSpaceY - (initialVelocity * time)) / (time * time);
+    return accelY;
+}
+
+int velocityEqX(float finalVelocityX, float initialVelocity, float time, float accelX) {
+    finalVelocityX = initialVelocity + (accelX * time);
+    return finalVelocityX;
+}
+
+int velocityEqY(float finalVelocityY, float initialVelocity, float time, float accelY) {
+    finalVelocityY = initialVelocity + (accelY * time);
+    return finalVelocityY;
+}
+
+double calcDistRoboBola (double xR, double yR, double xB, double yB, double tempo) {
     double dist = sqrt(pow((xB-xR),2) + pow((yB-yR),2));
     return dist;
 }
 
-// Calcula a aceleração necessária para o robor alcançar a bola
-double calcAceleraRobo (double xR, double yR, double xB, double yB, double tempo){
+double calcAceleraRobo (double xR, double yR, double xB, double yB, double tempo) {
     double dist = distRoboBola (xR, yR, xB, yB, tempo);
     double aclr = dist*2/pow(tempo,2);
     return aclr;
 }
 
-// Calcula a maior velocidade necessária para o robo alcançar a bola
-double calcVeloRobo (double xR, double yR, double xB, double yB, double tempo){
+double calcVeloRobo (double xR, double yR, double xB, double yB, double tempo) {
     double aclr = calcAceleraRobo (xR, yR, xB, yB, tempo);
     double velocidade = aclr*tempo;
     return velocidade;
-}
-
-int main(void) {
-    
 }
